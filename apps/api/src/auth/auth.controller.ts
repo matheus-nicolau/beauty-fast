be3155bfca2e4dd3a73e79c908600d55143f2ba9
matheus-nicolau <matheusnicolau.dev@gmail.com>
2025-09-11 21:00:50 -0400

    feat: implements the jwt service an sigin service.

--- apps/api/src/auth/auth.controller.ts
+++ apps/api/src/auth/auth.controller.ts
@@ -1,12 +1,14 @@
-import { Body, Controller, Post } from '@nestjs/common';
+import { Body, Controller, Post, Res } from '@nestjs/common';
 import { AuthService } from './auth.service';
 import { SigInDTO } from './dto/sigin.dto';
+import type { Response } from 'express';
 
 @Controller('auth')
 export class AuthController {
   constructor(private readonly authService: AuthService) {}
   @Post()
-  async login(@Body() sigIn: SigInDTO) {
+  async login(@Res() resp: Response, @Body() sigIn: SigInDTO) {
     const respSigin = await this.authService.sigin(sigIn);
+    return resp.status(200).end(respSigin);
   }
 }
--- apps/api/src/auth/auth.service.ts
+++ apps/api/src/auth/auth.service.ts
@@ -1,7 +1,48 @@
-import { Injectable } from '@nestjs/common';
+import { Inject, Injectable, UnauthorizedException } from '@nestjs/common';
 import { SigInDTO } from './dto/sigin.dto';
+import { InjectRepository } from '@nestjs/typeorm';
+import { Repository } from 'typeorm';
+import { User } from 'src/domain/users/entities/user.entity';
+import { HashingService } from './hashing/hashing.service';
+import jwtConfig from './config/jwt.config';
+import type { ConfigType } from '@nestjs/config';
+import { JwtService } from '@nestjs/jwt';
 
 @Injectable()
 export class AuthService {
-  async sigin(sigIn: SigInDTO) {}
+  constructor(
+    @InjectRepository(User)
+    private readonly userRepository: Repository<User>,
+    @Inject(jwtConfig.KEY)
+    private readonly jwtConfiguration: ConfigType<typeof jwtConfig>,
+    private readonly hashingService: HashingService,
+    private readonly jwtService: JwtService,
+  ) {}
+
+  async sigin(sigIn: SigInDTO): Promise<string> {
+    const user = await this.userRepository.findOneBy({ email: sigIn.email });
+    if (!user) throw new UnauthorizedException('Email ou senha incorretos !');
+
+    const comparedPassword = await this.hashingService.compare(
+      sigIn.password,
+      user.password,
+    );
+    if (!comparedPassword)
+      throw new UnauthorizedException('Email ou senha incorretos !');
+
+    const accessToken = await this.jwtService.signAsync(
+      {
+        sub: user.id,
+        email: user.email,
+      },
+      {
+        audience: this.jwtConfiguration.audience,
+        issuer: this.jwtConfiguration.issuer,
+        secret: this.jwtConfiguration.secret,
+        expiresIn: this.jwtConfiguration.ttl,
+      },
+    );
+
+    return accessToken;
+  }
 }
