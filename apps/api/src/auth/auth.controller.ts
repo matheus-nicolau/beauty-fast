6431bff25e282bfbdb2c1c0749e79234a3b7b45c
matheus-nicolau <matheusnicolau.dev@gmail.com>
2025-09-19 20:57:09 -0400

    feat: remove te promise of test.

--- apps/api/src/auth/auth.controller.ts
+++ apps/api/src/auth/auth.controller.ts
@@ -9,7 +9,6 @@ export class AuthController {
   constructor(private readonly authService: AuthService) {}
   @Post('login')
   async login(@Res() resp: Response, @Body() sigIn: SigInDTO) {
-    await new Promise((res) => setTimeout(res, 10000));
     const respSigin = await this.authService.sigin(sigIn);
     return resp.status(HttpStatus.OK).json(respSigin);
   }
