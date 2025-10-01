41e73be7d982c6ba88f3f7ab938980a61140db3b
matheus-nicolau <matheusnicolau.dev@gmail.com>
2025-09-30 20:57:12 -0400

    feat: change the space in calendar.

--- apps/web/app/(dashboards)/calendar/page.tsx
+++ apps/web/app/(dashboards)/calendar/page.tsx
@@ -35,7 +35,7 @@ export default function Page() {
         </header>
         <div className="flex flex-1 flex-col gap-4 p-4">
           <div className="grid auto-rows-min gap-4 md:grid-cols-5">
-            {Array.from({ length: 20 }).map((_, i) => (
+            {Array.from({ length: 15 }).map((_, i) => (
               <div key={i} className="bg-muted/50 aspect-square rounded-xl" />
             ))}
           </div>
--- apps/web/components/calendars.tsx
+++ apps/web/components/calendars.tsx
@@ -5,9 +5,17 @@ import * as React from "react";
 import { Calendar } from "@/components/ui/calendar";
 
 export function Calendars() {
-  const today: Date = new Date();
-  const [date, setDate] = React.useState<Date | undefined>(today);
+  const today = new Date();
+  const todayUTC: Date = new Date(
+    Date.UTC(today.getUTCFullYear(), today.getUTCMonth(), today.getUTCDate())
+  );
+
+  todayUTC.setHours(todayUTC.getHours() - 3);
+  const [date, setDate] = React.useState<Date | undefined>(todayUTC);
   const bookedDates = Array.from({ length: 12 }, (_, i) => new Date(+i));
+  console.log(today);
+  console.log(todayUTC);
+  console.log(today.getHours());
 
   return (
     <Calendar
@@ -22,7 +30,7 @@ export function Calendars() {
       modifiersClassNames={{
         booked: "[&>button]:line-through opacity-100",
       }}
-      className="rounded-lg border shadow-sm"
+      className="rounded-lg border shadow-sm ml-4 sm:ml-0.5"
     />
   );
 }
